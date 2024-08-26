6e88f0a7fe3dda412987d4f1fca41d601e691587
Fayaz Shaikh <67692714+azadeutsche114@users.noreply.github.com>
2024-08-26 23:04:41 +0200

    Update Directing_Customers_to_Subscription_Through_App_Behavior_Analysis.py

--- Directing_Customers_to_Subscription_Through_App_Behavior_Analysis.py
+++ Directing_Customers_to_Subscription_Through_App_Behavior_Analysis.py
@@ -1,15 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Directing Customers to Subscription Through App Behavior Analysis
-# by www.IndianAIProduction.com<br>
-# Project Source: www.IndianAIProduction.com/directing-customers-to-subscription-through-financial-app-behavior-analysis-ml-project<br>
-# ML Projects: www.IndianAIProduction.com/machine-learning-projects<br>
-# Videos: www.YouTube.com/IndianAIProduction
 
 # # Goal of the project :
 
-# The "FinTech" company launch there android and iOS mobile base app and want to grow there business. 
+# The "AyuRythm" company launch there android and iOS mobile base app and want to grow there business. 
 # But there is problem how to recomended this app and offer who realy want to use it. 
 # So for that company desided to give free trial to each and every customer for 24 hours
 # and collect data from the customers. In this senariao, some customer purchase the app and someone not.
@@ -36,99 +28,99 @@ from dateutil import parser # convert time in date time data type
 # In[2]:
 
 
-fineTech_appData = pd.read_csv("Dataset/FineTech appData/FineTech_appData.csv")
+AyuRythm_appData = pd.read_csv("Dataset/FineTech appData/AyuRythm_appData.csv")
 
 
 # In[3]:
 
 
-fineTech_appData.shape
+AyuRythm_appData.shape
 
 
 # In[4]:
 
 
-fineTech_appData.head(6) # show fisrt 6 rows of fineTech_appData DataFrame  *****code 1
+AyuRythm_appData.head(6) # show fisrt 6 rows of AyuRythm_appData DataFrame  *****code 1
 
 
 # In[5]:
 
 
-fineTech_appData.tail(6) # show last 6 rows of fineTech_appData DataFrame  *****code 2
+AyuRythm_appData.tail(6) # show last 6 rows of AyuRythm_appData DataFrame  *****code 2
 
 
 # In[6]:
 
 
 for i in [1,2,3,4,5]:
-    print(fineTech_appData.loc[i,'screen_list'],'\n')
+    print(AyuRythm_appData.loc[i,'screen_list'],'\n')
 
 
 # In[7]:
 
 
-fineTech_appData.isnull().sum() # take summation of null values
+AyuRythm_appData.isnull().sum() # take summation of null values
 
 
 # In[8]:
 
 
-fineTech_appData.info() # brief inforamtion about Dataset
+AyuRythm_appData.info() # brief inforamtion about Dataset
 
 
 # In[9]:
 
 
-fineTech_appData.describe() # give the distribution of numerical variables  *****code 3
+AyuRythm_appData.describe() # give the distribution of numerical variables  *****code 3
 
 
 # In[10]:
 
 
 # Get the unique value of each columns and it's length
-features = fineTech_appData.columns
+features = AyuRythm_appData.columns
 for i in features:
     print("""Unique value of {}\n{}\nlen is {} \n........................\n
-          """.format(i, fineTech_appData[i].unique(), len(fineTech_appData[i].unique())))
+          """.format(i, AyuRythm_appData[i].unique(), len(AyuRythm_appData[i].unique())))
 
 
 # In[11]:
 
 
-fineTech_appData.dtypes
+AyuRythm_appData.dtypes
 
 
 # In[12]:
 
 
 #  hour data convert string to int
-fineTech_appData['hour'] = fineTech_appData.hour.str.slice(1,3).astype(int) 
+AyuRythm_appData['hour'] = AyuRythm_appData.hour.str.slice(1,3).astype(int) 
 
 
 # In[13]:
 
 
 # get data type of each columns
-fineTech_appData.dtypes
+AyuRythm_appData.dtypes
 
 
 # In[14]:
 
 
-fineTech_appData.columns
+AyuRythm_appData.columns
 
 
 # In[15]:
 
 
 # drop object dtype columns
-fineTech_appData2 = fineTech_appData.drop(['user', 'first_open', 'screen_list', 'enrolled_date'], axis = 1)
+AyuRythm_appData2 = AyuRythm_appData.drop(['user', 'first_open', 'screen_list', 'enrolled_date'], axis = 1)
 
 
 # In[16]:
 
 
-fineTech_appData2.head(6) # head of numeric dataFrame *****code 4
+AyuRythm_appData2.head(6) # head of numeric dataFrame *****code 4
 
 
 # # Data Visualization
@@ -141,20 +133,20 @@ fineTech_appData2.head(6) # head of numeric dataFrame *****code 4
 # Heatmap
 plt.figure(figsize=(16,9)) # heatmap size in ratio 16:9
 
-sns.heatmap(fineTech_appData2.corr(), annot = True, cmap ='coolwarm') # show heatmap
+sns.heatmap(AyuRythm_appData2.corr(), annot = True, cmap ='coolwarm') # show heatmap
 
-plt.title("Heatmap using correlation matrix of fineTech_appData2", fontsize = 25) # title of heatmap *****code 5
+plt.title("Heatmap using correlation matrix of AyuRythm_appData2", fontsize = 25) # title of heatmap *****code 5
 
 
-# ## Pairplot of fineTech_appData2
+# ## Pairplot of AyuRythm_appData2
 
 # In[18]:
 
 
-# Pailplot of fineTech_appData2 Dataset
+# Pailplot of AyuRythm_appData2 Dataset
 
 #%matplotlib qt5 # for show graph in seperate window
-sns.pairplot(fineTech_appData2, hue  = 'enrolled') # *****code 6
+sns.pairplot(AyuRythm_appData2, hue  = 'enrolled') # *****code 6
 
 
 # ## Countplot of enrolled
@@ -163,15 +155,15 @@ sns.pairplot(fineTech_appData2, hue  = 'enrolled') # *****code 6
 
 
 # Show counterplot of 'enrolled' feature
-sns.countplot(fineTech_appData.enrolled) # *****code 7
+sns.countplot(AyuRythm_appData.enrolled) # *****code 7
 
 
 # In[20]:
 
 
 # value enrolled and not enrolled customers
-print("Not enrolled user = ", (fineTech_appData.enrolled < 1).sum(), "out of 50000")
-print("Enrolled user = ",50000-(fineTech_appData.enrolled < 1).sum(),  "out of 50000")
+print("Not enrolled user = ", (AyuRythm_appData.enrolled < 1).sum(), "out of 50000")
+print("Enrolled user = ",50000-(AyuRythm_appData.enrolled < 1).sum(),  "out of 50000")
 
 
 # ## Histogram of each feature of fineTech_appData2
@@ -182,13 +174,13 @@ print("Enrolled user = ",50000-(fineTech_appData.enrolled < 1).sum(),  "out of 5
 # plot histogram 
 
 plt.figure(figsize = (16,9)) # figure size in ratio 16:9
-features = fineTech_appData2.columns # list of columns name
+features = AyuRythm_appData2.columns # list of columns name
 for i,j in enumerate(features): 
     plt.subplot(3,3,i+1) # create subplot for histogram
     plt.title("Histogram of {}".format(j), fontsize = 15) # title of histogram
     
-    bins = len(fineTech_appData2[j].unique()) # bins for histogram
-    plt.hist(fineTech_appData2[j], bins = bins, rwidth = 0.8, edgecolor = "y", linewidth = 2, ) # plot histogram
+    bins = len(AyuRythm_appData2[j].unique()) # bins for histogram
+    plt.hist(AyuRythm_appData2[j], bins = bins, rwidth = 0.8, edgecolor = "y", linewidth = 2, ) # plot histogram
     
 plt.subplots_adjust(hspace=0.5) # space between horixontal axes (subplots) *****code 8
 
@@ -210,8 +202,8 @@ for i,j in enumerate(features):
 sns.set() # set background dark grid
 plt.figure(figsize = (14,5))
 plt.title("Correlation all features with 'enrolled' ", fontsize = 20)
-fineTech_appData3 = fineTech_appData2.drop(['enrolled'], axis = 1) # drop 'enrolled' feature
-ax =sns.barplot(fineTech_appData3.columns,fineTech_appData3.corrwith(fineTech_appData2.enrolled)) # plot barplot 
+AyuRythm_appData3 = AyuRythm_appData2.drop(['enrolled'], axis = 1) # drop 'enrolled' feature
+ax =sns.barplot(AyuRythm_appData3.columns,AyuRythm_appData3.corrwith(AyuRythm_appData2.enrolled)) # plot barplot 
 ax.tick_params(labelsize=15, labelrotation = 20, color ="k") # decorate x & y ticks font *****code 9
 
 
@@ -220,96 +212,96 @@ ax.tick_params(labelsize=15, labelrotation = 20, color ="k") # decorate x & y ti
 
 # parsing object data into data time format
 
-fineTech_appData['first_open'] =[parser.parse(i) for i in fineTech_appData['first_open']]
+AyuRythm_appData['first_open'] =[parser.parse(i) for i in AyuRythm_appData['first_open']]
 
 
 # In[25]:
 
 
-fineTech_appData['enrolled_date'] =[parser.parse(i) if isinstance(i, str) else i for i in fineTech_appData['enrolled_date']]
+AyuRythm_appData['enrolled_date'] =[parser.parse(i) if isinstance(i, str) else i for i in AyuRythm_appData['enrolled_date']]
 
 
 # In[26]:
 
 
-fineTech_appData.dtypes
+AyuRythm_appData.dtypes
 
 
 # In[27]:
 
 
-fineTech_appData['time_to_enrolled']  = (fineTech_appData.enrolled_date - fineTech_appData.first_open).astype('timedelta64[h]')
+AyuRythm_appData['time_to_enrolled']  = (AyuRythm_appData.enrolled_date - AyuRythm_appData.first_open).astype('timedelta64[h]')
 
 
 # In[28]:
 
 
 # plot histogram
-plt.hist(fineTech_appData['time_to_enrolled'].dropna()) # *****code 10
+plt.hist(AyuRythm_appData['time_to_enrolled'].dropna()) # *****code 10
 
 
 # In[29]:
 
 
 # Plot histogram
-plt.hist(fineTech_appData['time_to_enrolled'].dropna(), range = (0,100)) # *****code 11
+plt.hist(AyuRythm_appData['time_to_enrolled'].dropna(), range = (0,100)) # *****code 11
 
 
 # In[30]:
 
 
 # Those customers have enrolled after 48 hours set as 0
-fineTech_appData.loc[fineTech_appData.time_to_enrolled > 48, 'enrolled'] = 0
+AyuRythm_appData.loc[AyuRythm_appData.time_to_enrolled > 48, 'enrolled'] = 0
 
 
 # In[31]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[32]:
 
 
-fineTech_appData.drop(columns = ['time_to_enrolled', 'enrolled_date', 'first_open'], inplace=True)
+AyuRythm_appData.drop(columns = ['time_to_enrolled', 'enrolled_date', 'first_open'], inplace=True)
 
 
 # In[33]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[34]:
 
 
 # read csv file and convert it into numpy array
-fineTech_app_screen_Data = pd.read_csv("Dataset/FineTech appData/top_screens.csv").top_screens.values
+AyuRythm_app_screen_Data = pd.read_csv("Dataset/AyuRythm appData/top_screens.csv").top_screens.values
 
 
 # In[35]:
 
 
-fineTech_app_screen_Data
+AyuRythm_app_screen_Data
 
 
 # In[36]:
 
 
-type(fineTech_app_screen_Data)
+type(AyuRythm_app_screen_Data)
 
 
 # In[37]:
 
 
 # Add ',' at the end of each string of  'sreen_list' for further operation.
-fineTech_appData['screen_list'] = fineTech_appData.screen_list.astype(str) + ','
+AyuRythm_appData['screen_list'] = fineTech_appData.screen_list.astype(str) + ','
 
 
 # In[38]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[39]:
@@ -317,50 +309,50 @@ fineTech_appData
 
 # string into to number
 
-for screen_name in fineTech_app_screen_Data:
-    fineTech_appData[screen_name] = fineTech_appData.screen_list.str.contains(screen_name).astype(int)
-    fineTech_appData['screen_list'] = fineTech_appData.screen_list.str.replace(screen_name+",", "")
+for screen_name in AyuRythm_app_screen_Data:
+    AyuRythm_appData[screen_name] = AyuRythm_appData.screen_list.str.contains(screen_name).astype(int)
+    AyuRythm_appData['screen_list'] = AyuRythm_appData.screen_list.str.replace(screen_name+",", "")
 
 
 # In[40]:
 
 
 # test
-fineTech_appData.screen_list.str.contains('Splash').astype(int)
+AyuRythm_appData.screen_list.str.contains('Splash').astype(int)
 
 
 # In[41]:
 
 
 # test
-fineTech_appData.screen_list.str.replace('Splash'+",", "")
+AyuRythm_appData.screen_list.str.replace('Splash'+",", "")
 
 
 # In[42]:
 
 
 # get shape
-fineTech_appData.shape
+AyuRythm_appData.shape
 
 
 # In[43]:
 
 
 # head of DataFrame
-fineTech_appData.head(6) # *****code 12
+AyuRythm_appData.head(6) # *****code 12
 
 
 # In[44]:
 
 
 # remain screen in 'screen_list'
-fineTech_appData.loc[0,'screen_list']
+AyuRythm_appData.loc[0,'screen_list']
 
 
 # In[45]:
 
 
-fineTech_appData.screen_list.str.count(",").head(6)
+AyuRythm_appData.screen_list.str.count(",").head(6)
 
 
 # In[46]:
@@ -368,27 +360,27 @@ fineTech_appData.screen_list.str.count(",").head(6)
 
 # count remain screen list and store counted number in 'remain_screen_list'
 
-fineTech_appData['remain_screen_list'] = fineTech_appData.screen_list.str.count(",")
+AyuRythm_appData['remain_screen_list'] = AyuRythm_appData.screen_list.str.count(",")
 
 
 # In[47]:
 
 
 # Drop the 'screen_list'
-fineTech_appData.drop(columns = ['screen_list'], inplace=True)
+AyuRythm_appData.drop(columns = ['screen_list'], inplace=True)
 
 
 # In[48]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[49]:
 
 
 # total columns
-fineTech_appData.columns
+AyuRythm_appData.columns
 
 
 # In[50]:
@@ -406,14 +398,14 @@ saving_screens = ['Saving1',
                   'Saving9',
                   'Saving10',
                  ]
-fineTech_appData['saving_screens_count'] = fineTech_appData[saving_screens].sum(axis = 1)
-fineTech_appData.drop(columns = saving_screens, inplace = True)
+AyuRythm_appData['saving_screens_count'] = AyuRythm_appData[saving_screens].sum(axis = 1)
+AyuRythm_appData.drop(columns = saving_screens, inplace = True)
 
 
 # In[51]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[52]:
@@ -425,14 +417,14 @@ credit_screens = ['Credit1',
                   'Credit3Container',
                   'Credit3Dashboard',
                  ]
-fineTech_appData['credit_screens_count'] = fineTech_appData[credit_screens].sum(axis = 1)
-fineTech_appData.drop(columns = credit_screens, axis = 1, inplace = True)
+AyuRythm_appData['credit_screens_count'] = AyuRythm_appData[credit_screens].sum(axis = 1)
+AyuRythm_appData.drop(columns = credit_screens, axis = 1, inplace = True)
 
 
 # In[53]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[54]:
@@ -442,14 +434,14 @@ cc_screens = ['CC1',
               'CC1Category',
               'CC3',
              ]
-fineTech_appData['cc_screens_count'] = fineTech_appData[cc_screens].sum(axis = 1)
-fineTech_appData.drop(columns = cc_screens, inplace = True)
+AyuRythm_appData['cc_screens_count'] = AyuRythm_appData[cc_screens].sum(axis = 1)
+AyuRythm_appData.drop(columns = cc_screens, inplace = True)
 
 
 # In[55]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[56]:
@@ -460,32 +452,32 @@ loan_screens = ['Loan',
                 'Loan3',
                 'Loan4',
                ]
-fineTech_appData['loan_screens_count'] = fineTech_appData[loan_screens].sum(axis = 1)
-fineTech_appData.drop(columns = loan_screens, inplace = True)
+AyuRythm_appData['loan_screens_count'] = fineTech_appData[loan_screens].sum(axis = 1)
+AyuRythm_appData.drop(columns = loan_screens, inplace = True)
 
 
 # In[57]:
 
 
-fineTech_appData
+AyuRythm_appData
 
 
 # In[58]:
 
 
-fineTech_appData.shape
+AyuRythm_appData.shape
 
 
 # In[59]:
 
 
-fineTech_appData.info()
+AyuRythm_appData.info()
 
 
 # In[60]:
 
 
-fineTech_appData.describe()
+AyuRythm_appData.describe()
 
 
 # ## Heatmap with correlation matrix of new fineTech_appData
@@ -496,32 +488,32 @@ fineTech_appData.describe()
 # Heatmap with correlation matrix of new fineTech_appData
 
 plt.figure(figsize = (25,16)) 
-sns.heatmap(fineTech_appData.corr(), annot = True, linewidth =2) #*****code 13
+sns.heatmap(AyuRythm_appData.corr(), annot = True, linewidth =2) #*****code 13
 
 
 # In[62]:
 
 
-fineTech_appData.columns
+AyuRythm_appData.columns
 
 
 # In[63]:
 
 
-fineTech_appData['ProfileChildren '].unique()
+AyuRythm_appData['ProfileChildren '].unique()
 
 
 # In[64]:
 
 
-corr_matrix = fineTech_appData.corr()
+corr_matrix = AyuRythm_appData.corr()
 corr_matrix['ProfileChildren ']
 
 
 # In[65]:
 
 
-fineTech_appData['ProfileChildren ']
+AyuRythm_appData['ProfileChildren ']
 
 
 # # Data Preprocessing
@@ -531,16 +523,16 @@ fineTech_appData['ProfileChildren ']
 # In[66]:
 
 
-clean_fineTech_appData = fineTech_appData
-target = fineTech_appData['enrolled'] 
-fineTech_appData.drop(columns = 'enrolled', inplace = True)
+clean_AyuRythm_appData = AyuRythm_appData
+target = AyuRythm_appData['enrolled'] 
+AyuRythm_appData.drop(columns = 'enrolled', inplace = True)
 
 
 # In[67]:
 
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(fineTech_appData, target, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(AyuRythm_appData, target, test_size = 0.2, random_state = 0)
 
 
 # In[68]:
@@ -872,10 +864,10 @@ final_result
 import pickle
 
 # save model
-pickle.dump(xgb_model_pt2, open('FineTech_app_ML_model.pickle', 'wb'))
+pickle.dump(xgb_model_pt2, open('AyuRythm_ML_model.pickle', 'wb'))
 
 # load model
-ml_model_pl = pickle.load(open('FineTech_app_ML_model.pickle', 'rb'))
+ml_model_pl = pickle.load(open('AyuRythm_app_ML_model.pickle', 'rb'))
 
 # predict the output
 y_pred_pl = ml_model_pl.predict(X_test_sc)
@@ -895,10 +887,10 @@ print("Accuracy of model = ",accuracy_score(y_test, y_pred_pl))
 from sklearn.externals import joblib
 
 # save model
-joblib.dump(xgb_model_pt2, 'FineTech_app_ML_model.joblib')
+joblib.dump(xgb_model_pt2, 'AyuRythm_app_ML_model.joblib')
 
 # load model
-ml_model_jl = joblib.load('FineTech_app_ML_model.joblib')
+ml_model_jl = joblib.load('AyuRythm_app_ML_model.joblib')
 
 # predict the output 
 y_pred_jl = ml_model_jl.predict(X_test_sc)
@@ -907,10 +899,3 @@ cm_jl = confusion_matrix(y_test, y_pred_jl)
 print('Confussion matrix = \n', cm_jl)
 
 print("Accuracy of model = ", accuracy_score(y_test, y_pred_jl))
-
-
-# End ==================================================<br>
-# This Project Created By www.IndianAIProduction.com<br>
-# Project Source: www.IndianAIProduction.com/directing-customers-to-subscription-through-financial-app-behavior-analysis-ml-project<br>
-# ML Projects: www.IndianAIProduction.com/machine-learning-projects<br>
-# Videos: www.YouTube.com/IndianAIProduction
